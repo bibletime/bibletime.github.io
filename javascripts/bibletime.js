f94d462c18d1594930f87c471fb4f2963e2f17d7
Martin Gruner <martin.gruner@otrs.com>
2015-12-04 19:58:38 +0100

    Added human readable dates for the news articles.

--- javascripts/bibletime.js
+++ javascripts/bibletime.js
@@ -1,6 +1,12 @@
 $('document').ready( function() {
     $('.ReadMore').on('click', function() {
-        $(this).parent('p').hide().parents('article').find('.Hidden').show();
+        $(this).parent('p').hide().parents('article').find('.hidden').slideDown();
         return false;
     });
+    $.each($('div.date'), function() {
+        var ISODate = $(this).text();
+        $(this).attr('title', ISODate);
+        $(this).text(moment(ISODate, '"YYYY-MM-DD').fromNow());
+    } );
+
 });
